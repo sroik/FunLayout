f183f7594257bbb601fe96021c826819cc5f6bf3
Sroik <Sroik1994@gmail.com>
2016-04-28 16:31:29 +0300

    fix accessories

--- FunLayout.podspec
+++ FunLayout.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name         = "FunLayout"
-  s.version      = "0.1"
+  s.version      = "1.0"
   s.summary      = "funny auto layout"
 
   s.description  = <<-DESC
@@ -14,7 +14,7 @@ Pod::Spec.new do |s|
 
   s.platform     = :ios, "8.0"
 
-  s.source       = { :git => "https://github.com/Sroik/FunLayout.git", :tag => "0.1" }
+  s.source       = { :git => "https://github.com/Sroik/FunLayout.git", :tag => "1.0" }
   s.framework = "UIKit"
   s.source_files  =  "src/FunLayout/FunLayoutModule/*.{swift}"
 
--- src/FunLayout/FunLayoutModule/FunLayoutOperators.swift
+++ src/FunLayout/FunLayoutModule/FunLayoutOperators.swift
@@ -8,102 +8,103 @@
 
 import UIKit
 
+/*
+* Below usage formula:
+* firstItem.fun_attribute {== or ~, <=, >=} secondItem.fun_attribute {*, /} multiplier {+, -} constant
+*/
+
+
+infix operator ~ {
+/* same as '==' operator */
+    associativity left
+    precedence 90
+}
 
 /*
- * Below usage formula:
- * firstItem.fun_attribute {== or ~, <=, >=} secondItem.fun_attribute {*, /} multiplier {+, -} constant
- */
+* Why duplicated code?
+* Because I wanna receive compile error,
+* when I use wrong type
+* neither FunLayoutAttribute, UIView nor CGFloat
+*/
 
-infix operator ~ { associativity left precedence 90 } //same as '==' operator
-infix operator == { associativity left precedence 90 }
-infix operator >= { associativity left precedence 91 }
-infix operator <= { associativity left precedence 92 }
-
-/* 
- * Why duplicated code?
- * Because I wanna receive compile error, 
- * when I use wrong type
- * neither FunLayoutAttribute, UIView nor CGFloat
- */
-
-func ~(left: FunLayoutAttribute, right: FunLayoutAttribute) -> [NSLayoutConstraint] {
+public func ~(left: FunLayoutAttribute, right: FunLayoutAttribute) -> [NSLayoutConstraint] {
     return fun_activateConstraints(leftLayoutAttribute:left, right: right, relation: .Equal)
 }
 
-func ~(left: FunLayoutAttribute, right: UIView) -> [NSLayoutConstraint] {
+public func ~(left: FunLayoutAttribute, right: UIView) -> [NSLayoutConstraint] {
     return fun_activateConstraints(leftLayoutAttribute:left, right: right, relation: .Equal)
 }
 
-func ~(left: FunLayoutAttribute, right: CGFloat) -> [NSLayoutConstraint] {
+public func ~(left: FunLayoutAttribute, right: CGFloat) -> [NSLayoutConstraint] {
     return fun_activateConstraints(leftLayoutAttribute:left, right: right, relation: .Equal)
 }
 
-func ==(left: FunLayoutAttribute, right: FunLayoutAttribute) -> [NSLayoutConstraint] {
+public func ==(left: FunLayoutAttribute, right: FunLayoutAttribute) -> [NSLayoutConstraint] {
     return (left ~ right)
 }
 
-func ==(left: FunLayoutAttribute, right: UIView) -> [NSLayoutConstraint] {
+public func ==(left: FunLayoutAttribute, right: UIView) -> [NSLayoutConstraint] {
     return (left ~ right)
 }
 
-func ==(left: FunLayoutAttribute, right: CGFloat) -> [NSLayoutConstraint] {
+public func ==(left: FunLayoutAttribute, right: CGFloat) -> [NSLayoutConstraint] {
     return (left ~ right)
 }
 
 //MARK:- less than or equal
 
-func <=(left: FunLayoutAttribute, right: FunLayoutAttribute) -> [NSLayoutConstraint] {
+public func <=(left: FunLayoutAttribute, right: FunLayoutAttribute) -> [NSLayoutConstraint] {
     return fun_activateConstraints(leftLayoutAttribute:left, right: right, relation: .LessThanOrEqual)
 }
 
-func <=(left: FunLayoutAttribute, right: UIView) -> [NSLayoutConstraint] {
+public func <=(left: FunLayoutAttribute, right: UIView) -> [NSLayoutConstraint] {
     return fun_activateConstraints(leftLayoutAttribute:left, right: right, relation: .LessThanOrEqual)
 }
 
-func <=(left: FunLayoutAttribute, right: CGFloat) -> [NSLayoutConstraint] {
+public func <=(left: FunLayoutAttribute, right: CGFloat) -> [NSLayoutConstraint] {
     return fun_activateConstraints(leftLayoutAttribute:left, right: right, relation: .LessThanOrEqual)
 }
 
 //MARK:- great than or equal
 
-func >=(left: FunLayoutAttribute, right: FunLayoutAttribute) -> [NSLayoutConstraint] {
+public func >=(left: FunLayoutAttribute, right: FunLayoutAttribute) -> [NSLayoutConstraint] {
     return fun_activateConstraints(leftLayoutAttribute:left, right: right, relation: .GreaterThanOrEqual)
 }
 
-func >=(left: FunLayoutAttribute, right: UIView) -> [NSLayoutConstraint] {
+public func >=(left: FunLayoutAttribute, right: UIView) -> [NSLayoutConstraint] {
     return fun_activateConstraints(leftLayoutAttribute:left, right: right, relation: .GreaterThanOrEqual)
 }
 
-func >=(left: FunLayoutAttribute, right: CGFloat) -> [NSLayoutConstraint] {
+public func >=(left: FunLayoutAttribute, right: CGFloat) -> [NSLayoutConstraint] {
     return fun_activateConstraints(leftLayoutAttribute:left, right: right, relation: .GreaterThanOrEqual)
 }
 
 //MARK:- multiplier operators
 
-func *(left: FunLayoutAttribute, right: CGFloat) -> FunLayoutAttribute {
+public func *(left: FunLayoutAttribute, right: CGFloat) -> FunLayoutAttribute {
     var layoutAttribute = left
     layoutAttribute.multiplier = right
     
     return layoutAttribute
 }
 
-func /(left: FunLayoutAttribute, right: CGFloat) -> FunLayoutAttribute {
+public func /(left: FunLayoutAttribute, right: CGFloat) -> FunLayoutAttribute {
     var layoutAttribute = left
     layoutAttribute.multiplier = 1.0/right
-
+    
     return layoutAttribute
 }
 
 //MARK:- constants operators
 
-func +(left: FunLayoutAttribute, right: CGFloat) -> FunLayoutAttribute {
+public func +(left: FunLayoutAttribute, right: CGFloat) -> FunLayoutAttribute {
     var layoutAttribute = left
     layoutAttribute.constant = right
     
     return layoutAttribute
 }
 
-func -(left: FunLayoutAttribute, right: CGFloat) -> FunLayoutAttribute {
+public func -(left: FunLayoutAttribute, right: CGFloat) -> FunLayoutAttribute {
     var layoutAttribute = left
     layoutAttribute.constant = -right
     
