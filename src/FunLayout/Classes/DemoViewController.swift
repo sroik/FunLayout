4f081574e08b168036d9b68b2f15887d276a38e8
Sroik <Sroik1994@gmail.com>
2016-04-28 17:32:52 +0300

    add examples views

--- src/FunLayout/Classes/DemoViewController.swift
+++ src/FunLayout/Classes/DemoViewController.swift
@@ -10,68 +10,71 @@ import UIKit
 
 class DemoViewController: UIViewController {
 
+    lazy var whiteView = UIView()
+    lazy var greenView = UIView()
+    lazy var blackView = UIView()
+    lazy var redView = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.runDemo()
+        self.setupWhiteView()
+        self.setupGreenView()
+        self.setupBlackView()
+        self.setupRedView()
     }
     
 //MARK:- demo running
     
-    func runDemo() {
+    func setupWhiteView() {
+        whiteView.backgroundColor = UIColor.whiteColor()
+        whiteView.translatesAutoresizingMaskIntoConstraints = false // You can use .fun_prepareForLayout() function
+        self.view.addSubview(whiteView)
         
-        /* background white view configurations */
-        
-        let backgroundView = UIView()
-        backgroundView.backgroundColor = UIColor.whiteColor()
-        backgroundView.translatesAutoresizingMaskIntoConstraints = false // You can use .fun_prepareForLayout() function
-        self.view.addSubview(backgroundView)
-
-        backgroundView.fun_edges == self.view
-        
-        /* background green view configurations */
-        
-        let greenView = UIView()
+        whiteView.fun_edges == self.view
+    }
+    
+    func setupGreenView() {
         greenView.backgroundColor = UIColor.greenColor()
         greenView.fun_prepareForLayout()
         self.view.addSubview(greenView)
         
         greenView.fun_size == self.view.fun_size*0.5
         greenView.fun_center == self.view
+    }
+    
+    func setupBlackView() {
+        blackView.backgroundColor = UIColor.blackColor()
+        blackView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(blackView)
         
-        /* black box configurations */
-        
-        let blackBox = UIView()
-        blackBox.backgroundColor = UIColor.blackColor()
-        blackBox.translatesAutoresizingMaskIntoConstraints = false
-        self.view.addSubview(blackBox)
-        
-        blackBox.fun_width == self.view.fun_width/4.0 + 50.0
-        blackBox.fun_center == self.view
-        blackBox.fun_height >= CGFloat(150.0)
+        blackView.fun_width == self.view.fun_width/4.0 + 50.0
+        blackView.fun_center == self.view
+        blackView.fun_height >= CGFloat(150.0)
         
         let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(3.0 * Double(NSEC_PER_SEC)))
         dispatch_after(delayTime, dispatch_get_main_queue()) {
-            blackBox.fun_height == 400.0
-            blackBox.fun_width == self.view.fun_width*0.8 - 100.0
-            blackBox.fun_centerX == self.view.fun_centerX + 50.0
-
+            [unowned self] in
+            
+            self.blackView.fun_height == 400.0
+            self.blackView.fun_width == self.view.fun_width*0.8 - 100.0
+            self.blackView.fun_centerX == self.view.fun_centerX + 50.0
+            
             self.view.fun_layoutIfNeededWithDuration(0.5)
         }
-
-        /* configure redLine view */
-
-        let redLine = UIView()
+    }
+    
+    func setupRedView() {
+        let redView = UIView()
         
-        redLine.backgroundColor = UIColor.redColor()
-        self.view.addSubview(redLine)
+        redView.backgroundColor = UIColor.redColor()
+        self.view.addSubview(redView)
         
-        redLine.fun_prepareForLayout()
-        redLine.fun_bottom == blackBox.fun_top - 10.0
-        redLine.fun_height == 20.0
-        redLine.fun_centerX == blackBox
-        redLine.fun_width == blackBox
-        
+        redView.fun_prepareForLayout()
+        redView.fun_bottom == blackView.fun_top - 10.0
+        redView.fun_height == 20.0
+        redView.fun_centerX == blackView
+        redView.fun_width == blackView
     }
 
 }
